d484293e59ed7e71358d019520c3f8816c6faee1
Fabio Riccardi <fabio@glass-imaging.com>
2023-01-27 16:42:36 -0800

    Added support for color correction on a grid of points.

--- CLImageTest/MonitorCalibration/monitorCalibration.cpp
+++ CLImageTest/MonitorCalibration/monitorCalibration.cpp
@@ -10,9 +10,44 @@
 #include <regex>
 #include <vector>
 #include <fstream>
-#include <iostream>
+#include <cmath>
 
 #include "gls_image.hpp"
+
+struct monitor_response {
+    gls::point coordinates;
+    std::array<gls::rgb_pixel_16, 256> data;
+};
+
+constexpr const int inset = 300;
+std::array<std::array<gls::point, 3>, 3> computeCalibrationCoordinates(const gls::image<gls::rgb_pixel_16>& inputImage) {
+    return {{
+        {{{ inset, inset }, { inputImage.width / 2, inset }, { inputImage.width - inset, inset }}},
+        {{{ inset, inputImage.height / 2 }, { inputImage.width / 2, inputImage.height / 2 }, { inputImage.width - inset, inputImage.height / 2 }}},
+        {{{ inset, inputImage.height - inset }, { inputImage.width / 2, inputImage.height - inset }, { inputImage.width - inset, inputImage.height - inset }}}
+    }};
+}
+
+void cantParseFile(const std::filesystem::path& p) {
+    std::cerr << "Couldn't read calibration file: " << p.string() << std::endl;
+}
+
+gls::rgb_pixel_16 lookup(const std::array<gls::rgb_pixel_16, 256>& lut, const gls::rgb_pixel_16& val) {
+    gls::rgb_pixel_16 result;
+    for (int c = 0; c < 3; c++) {
+        const uint8_t val8 = val[c] >> 8;
+
+        if (val8 > 0) {
+            // For input values greater than the minimum LUT value, interpolate the 8 bit LUT entry over 16 bit data
+            const float ratio = lut[val8][c] / (val[c] / 256.0f);
+            result[c] = std::clamp(val[c] * ratio, 0.0f, (float) 0xffff);
+        } else {
+            // Oterwise just use the value from the LUT
+            result[c] = lut[val8][c] << 8;
+        }
+    }
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
     auto input_path = std::filesystem::path(argv[1]);
@@ -23,8 +58,8 @@ int main(int argc, const char * argv[]) {
 
     const auto monitor_calibration_file = input_dir / "MonitorCalibration.dat";
 
-    // Monitor calibration data
-    gls::rgb_pixel_16 lut[256];
+    const constexpr gls::size calibration_grid = {3, 3};
+    std::array<std::array<monitor_response, calibration_grid.width>, calibration_grid.height> calibration_data;
 
     if (!exists(status(monitor_calibration_file))) {
         std::vector<std::filesystem::path> directory_listing;
@@ -56,30 +91,42 @@ int main(int argc, const char * argv[]) {
                     exposureIndex = std::atoi(regex_match[3].str().c_str());
                 }
 
+                std::cout << "Processing file " << filename << ", captureNumber: " << captureNumber << ", pixelLevel: " << pixelLevel << ", exposureIndex: " << exposureIndex << std::endl;
+
                 const auto inputImage = gls::image<gls::rgb_pixel_16>::read_png_file(input_path.string());
 
-                gls::Vector<3> pixelValue = gls::Vector<3>::zeros();
-                for (int y = -2; y <= 2; y++) {
-                    for (int x = -2; x <= 2; x++) {
-                        pixelValue += gls::Vector<3>((*inputImage)[inputImage->height/2 + y][inputImage->width/2 + x].v);
+                const auto calibration_coordinates = computeCalibrationCoordinates(*inputImage);
+
+                for (int cg_y = 0; cg_y < calibration_grid.height; cg_y++) {
+                    for (int cg_x = 0; cg_x < calibration_grid.width; cg_x++) {
+                        const auto& p = calibration_data[cg_y][cg_x].coordinates = calibration_coordinates[cg_y][cg_x];
+                        auto& lut = calibration_data[cg_y][cg_x].data;
+
+                        gls::Vector<3> pixelValue = gls::Vector<3>::zeros();
+                        for (int y = -2; y <= 2; y++) {
+                            for (int x = -2; x <= 2; x++) {
+                                pixelValue += gls::Vector<3>((*inputImage)[p.y + y][p.x + x].v);
+                            }
+                        }
+                        pixelValue /= 25.0f;
+                        lut[pixelLevel] = pixelValue / 256.0f;
                     }
                 }
-                pixelValue /= 25.0f;
-                lut[pixelLevel] = pixelValue / 256.0f;
-
-                std::cout << "Processing file " << filename << ", captureNumber: " << captureNumber << ", pixelLevel: " << pixelLevel << ", exposureIndex: " << exposureIndex << std::endl;
             }
         }
 
         // Make sure lookup table data is monotonic
-        {
-            gls::rgb_pixel_16 last_value = lut[255];
-            for (int i = 254; i >= 0; i--) {
-                for (int c = 0; c < 3; c++) {
-                    if (lut[i][c] > last_value[c]) {
-                        lut[i][c] = last_value[c];
-                    } else {
-                        last_value[c] = lut[i][c];
+        for (int cg_y = 0; cg_y < calibration_grid.height; cg_y++) {
+            for (int cg_x = 0; cg_x < calibration_grid.width; cg_x++) {
+                auto& lut = calibration_data[cg_y][cg_x].data;
+                gls::rgb_pixel_16 last_value = lut[255];
+                for (int i = 254; i >= 0; i--) {
+                    for (int c = 0; c < 3; c++) {
+                        if (lut[i][c] > last_value[c]) {
+                            lut[i][c] = last_value[c];
+                        } else {
+                            last_value[c] = lut[i][c];
+                        }
                     }
                 }
             }
@@ -87,76 +134,115 @@ int main(int argc, const char * argv[]) {
 
         // Save monitor calibration data to file
         std::fstream monitor_calibration { monitor_calibration_file, monitor_calibration.out };
-        for (int i = 0; i < 256; i++) {
-            monitor_calibration << i << " " << lut[i].red << " " << lut[i].green << " " << lut[i].blue << std::endl;
+        monitor_calibration << calibration_grid.width << " " << calibration_grid.height << std::endl;
+        for (int cg_y = 0; cg_y < calibration_grid.height; cg_y++) {
+            for (int cg_x = 0; cg_x < calibration_grid.width; cg_x++) {
+                monitor_calibration << calibration_data[cg_y][cg_x].coordinates.x << " " << calibration_data[cg_y][cg_x].coordinates.y << std::endl;
+
+                const auto& lut = calibration_data[cg_y][cg_x].data;
+                for (int i = 0; i < 256; i++) {
+                    monitor_calibration << i << " " << lut[i].red << " " << lut[i].green << " " << lut[i].blue << std::endl;
+                }
+            }
         }
     } else {
         std::ifstream monitor_calibration { monitor_calibration_file, std::ios::in };
 
-        int index = 0, last_index = -1;
-        int red, green, blue;
-        while (monitor_calibration >> index >> red >> green >> blue) {
-            // Perform some input data validation
-            if ((index != last_index + 1) || (red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255)) {
-                std::cerr << "Couldn't read calibration file: " << monitor_calibration_file.string() << std::endl;
-                return -1;
-            }
-            last_index = index;
-
-            lut[index] = { (uint16_t) red, (uint16_t) green, (uint16_t) blue};
-        }
-        if (index != 255) {
-            std::cerr << "Couldn't read calibration file: " << monitor_calibration_file.string() << std::endl;
+        gls::size grid;
+        if (!(monitor_calibration >> grid.width >> grid.height)) {
+            cantParseFile(monitor_calibration_file);
             return -1;
         }
-    }
 
-    // Pretty print display+camera transfer function
-    for (int i = 0; i < 256; i++) {
-        std::cout << "level " << i << ":\t" << lut[i].red << ", " << lut[i].green << ", " << lut[i].blue << std::endl;
+        assert( grid == calibration_grid );
+
+        for (int cg_y = 0; cg_y < calibration_grid.height; cg_y++) {
+            for (int cg_x = 0; cg_x < calibration_grid.width; cg_x++) {
+                std::cout << "Reading grid element " << cg_x << ", " << cg_y << std::endl;
+
+                if (!(monitor_calibration >> calibration_data[cg_y][cg_x].coordinates.x >> calibration_data[cg_y][cg_x].coordinates.y)) {
+                    cantParseFile(monitor_calibration_file);
+                    return -1;
+                }
+
+                auto& lut = calibration_data[cg_y][cg_x].data;
+
+                int index = 0, last_index = -1;
+                int red, green, blue;
+                for (int i = 0; i < 256; i++) {
+                    if (monitor_calibration >> index >> red >> green >> blue) {
+                        // Perform some input data validation
+                        if ((index != last_index + 1) || (red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255)) {
+                            cantParseFile(monitor_calibration_file);
+                            return -1;
+                        }
+                        last_index = index;
+
+                        lut[index] = { (uint16_t) red, (uint16_t) green, (uint16_t) blue};
+                    } else {
+                        cantParseFile(monitor_calibration_file);
+                        return -1;
+                    }
+                }
+            }
+        }
     }
 
     // Build inverse transfer function to linearize the display+camera response
-    gls::rgb_pixel_16 inv_lut[256];
-    std::memset(inv_lut, 0, sizeof(inv_lut));
+    std::array<std::array<std::array<gls::rgb_pixel_16, 256>, 3>, 3> inv_lut_grid;
+    std::memset(inv_lut_grid.data(), 0, sizeof(inv_lut_grid));
 
-    // Fill the inverse LUT with available data
-    for (int i = 0; i < 256; i++) {
-        const auto entry = lut[i];
-        for (int c = 0; c < 3; c++) {
-            inv_lut[entry[c]][c] = i;
-        }
-    }
+    for (int cg_y = 0; cg_y < calibration_grid.height; cg_y++) {
+        for (int cg_x = 0; cg_x < calibration_grid.width; cg_x++) {
+            std::cout << "Computing inverse LUT at " << cg_x << ", " << cg_y << std::endl;
 
-    // Interpolate table data for missing values
-    gls::rgb_pixel_16 last_value = {0, 0, 0};
-    gls::rgb_pixel_16 last_index = {0, 0, 0};
-    for (int i = 0; i < 256; i++) {
-        auto entry = inv_lut[i];
-        if (i == 255) {
-            for (int c = 0; c < 3; c++) {
-                if (entry[c] == 0) {
-                    entry[c] = 255;
+            const auto& lut = calibration_data[cg_y][cg_x].data;
+            auto& inv_lut = inv_lut_grid[cg_y][cg_x];
+
+            // Pretty print display+camera transfer function
+            for (int i = 0; i < 256; i++) {
+                std::cout << "level " << i << ":\t" << lut[i].red << ", " << lut[i].green << ", " << lut[i].blue << std::endl;
+            }
+
+            // Fill the inverse LUT with available data
+            for (int i = 0; i < 256; i++) {
+                const auto entry = lut[i];
+                for (int c = 0; c < 3; c++) {
+                    inv_lut[entry[c]][c] = i;
                 }
             }
-        }
-        for (int c = 0; c < 3; c++) {
-            if (entry[c] != 0) {
-                if (i - last_index[c] > 1) {
-                    float delta = (entry[c] - last_value[c]) / (float) (i - last_index[c]);
-                    for (int j = 0; j < i - last_index[c]; j++) {
-                        inv_lut[last_index[c] + j][c] = last_value[c] + delta * j;
+
+            // Interpolate table data for missing values
+            gls::rgb_pixel_16 last_value = {0, 0, 0};
+            gls::rgb_pixel_16 last_index = {0, 0, 0};
+            for (int i = 0; i < 256; i++) {
+                auto entry = inv_lut[i];
+                if (i == 255) {
+                    for (int c = 0; c < 3; c++) {
+                        if (entry[c] == 0) {
+                            entry[c] = 255;
+                        }
                     }
                 }
-                last_index[c] = i;
-                inv_lut[i][c] = last_value[c] = entry[c];
+                for (int c = 0; c < 3; c++) {
+                    if (entry[c] != 0) {
+                        if (i - last_index[c] > 1) {
+                            float delta = (entry[c] - last_value[c]) / (float) (i - last_index[c]);
+                            for (int j = 0; j < i - last_index[c]; j++) {
+                                inv_lut[last_index[c] + j][c] = last_value[c] + delta * j;
+                            }
+                        }
+                        last_index[c] = i;
+                        inv_lut[i][c] = last_value[c] = entry[c];
+                    }
+                }
+            }
+
+            // Pretty print display+camera inverse transfer function
+            for (int i = 0; i < 256; i++) {
+                std::cout << "inv level " << i << ":\t" << inv_lut[i].red << ", " << inv_lut[i].green << ", " << inv_lut[i].blue << std::endl;
             }
         }
-    }
-
-    // Pretty print display+camera inverse transfer function
-    for (int i = 0; i < 256; i++) {
-        std::cout << "inv level " << i << ":\t" << inv_lut[i].red << ", " << inv_lut[i].green << ", " << inv_lut[i].blue << std::endl;
     }
 
     // Read image to transform
@@ -168,18 +254,34 @@ int main(int argc, const char * argv[]) {
     const auto inputImage = gls::image<gls::rgb_pixel_16>::read_png_file(input);
 
     // Apply inverse transfer function to captured data to recover the original color values
-    inputImage->apply([&inv_lut] (gls::rgb_pixel_16* p, int x, int y) {
-        for (int c = 0; c < 3; c++) {
-            const uint8_t val8 = (*p)[c] >> 8;
+    inputImage->apply([&inv_lut_grid, &inputImage, &calibration_data] (gls::rgb_pixel_16* p, int x, int y) {
+        const bool use_interpolation = true;
+        if (use_interpolation) {
+            gls::point grid_coordinates = {
+                std::clamp(x, inset, inputImage->width - inset - 1),
+                std::clamp(y, inset, inputImage->height - inset - 1)
+            };
 
-            if (val8 > 0) {
-                // For input values greater than the minimum LUT value, interpolate the 8 bit LUT entry over 16 bit data
-                const float ratio = inv_lut[val8][c] / ((*p)[c] / 256.0f);
-                (*p)[c] = std::clamp((*p)[c] * ratio, 0.0f, (float) 0xffff);
-            } else {
-                // Oterwise just use the value from the LUT
-                (*p)[c] = inv_lut[val8][c] << 8;
+            for (int cg_y = 0; cg_y < calibration_grid.height - 1; cg_y++) {
+                for (int cg_x = 0; cg_x < calibration_grid.width - 1; cg_x++) {
+                    const gls::rectangle quadrant(calibration_data[cg_y][cg_x].coordinates,
+                                                  calibration_data[cg_y+1][cg_x+1].coordinates);
+                    if (quadrant.contains(grid_coordinates)) {
+                        float wx = (grid_coordinates.x - quadrant.x) / (float) quadrant.width;
+                        float wy = (grid_coordinates.y - quadrant.y) / (float) quadrant.height;
+
+                        const auto p00 = lookup(inv_lut_grid[cg_y][cg_x], *p);
+                        const auto p10 = lookup(inv_lut_grid[cg_y][cg_x + 1], *p);
+                        const auto p01 = lookup(inv_lut_grid[cg_y + 1][cg_x], *p);
+                        const auto p11 = lookup(inv_lut_grid[cg_y + 1][cg_x + 1], *p);
+
+                        *p = lerp(lerp(p00, p10, wx),
+                                  lerp(p01, p11, wx), wy);
+                    }
+                }
             }
+        } else {
+            *p = lookup(inv_lut_grid[1][1], *p);
         }
     });
 
